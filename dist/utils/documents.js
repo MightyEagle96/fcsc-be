f980891b9c090127bf1974076df76e5bba62c555
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2025-09-10 15:53:38 +0100

    made changes

--- dist/utils/documents.js
+++ dist/utils/documents.js
@@ -5,7 +5,6 @@ const documents = [
     "Letter of First Appointment",
     "Gazette of Confirmation of Appointment",
     "Letter of Last Promotion",
-    "Last promotion letter",
     "Birth Certificate",
     "Professional Certificate (where applicable)",
     "Conversion/Transfer of Service/Regularization of Appointment (where applicable)",
--- src/utils/documents.ts
+++ src/utils/documents.ts
@@ -2,7 +2,7 @@ const documents = [
   "Letter of First Appointment",
   "Gazette of Confirmation of Appointment",
   "Letter of Last Promotion",
-  "Last promotion letter",
+
   "Birth Certificate",
   "Professional Certificate (where applicable)",
   "Conversion/Transfer of Service/Regularization of Appointment (where applicable)",
