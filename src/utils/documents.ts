6a9a49ab78f93cfa946218712898371a261c54fd
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2025-09-10 16:45:16 +0100

    made changes

--- src/utils/documents.ts
+++ src/utils/documents.ts
@@ -2,7 +2,6 @@ const documents = [
   "Letter of First Appointment",
   "Gazette of Confirmation of Appointment",
   "Letter of Last Promotion",
-
   "Birth Certificate",
   "Professional Certificate (where applicable)",
   "Conversion/Transfer of Service/Regularization of Appointment (where applicable)",
